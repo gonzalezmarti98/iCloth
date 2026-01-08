8ac4077df4b5ce4fb79b8138938e7e8d6fca6e51
Marti Gonzalez <martimartigonzalez@gmail.com>
2026-01-08 18:44:11 +0100

    Quitado algun comentario del codigo

--- app/src/main/java/com/martigonzalez/project_icloth/model/Prenda.kt
+++ app/src/main/java/com/martigonzalez/project_icloth/model/Prenda.kt
@@ -5,8 +5,8 @@ data class Prenda(
     var id: String = "",
     val nombre: String = "",
     val imagenUrl: String = "",
-    val categoria: String = "", // He aumentado bastante el array
-    val colorPpal: String = "", // Para el color seleccionado en el RecyclerView
+    val categoria: String = "",
+    val colorPpal: String = "",
     val ocasion: String = "",
     val temporada: String = "",
     val nivelFormalidad: String = "",
@@ -17,17 +17,3 @@ data class Prenda(
     // Constructor vacío requerido por Firestore
     constructor() : this("", "", "", "", "", "", "", "", "", "")
 }
-
-/*
-ATRIBUTOS que podríamos MODIFICAR:
-- val categoria --> que sea una lista que según lo q elijas, luego salga "val tipo: String"
-    ROPA_SUPERIOR - val tipo - camiseta corta, camiseta larga, camisa, chaqueta, jersey, polo, sudadera, térmica, top
-    ROPA_INFERIOR - val tipo - falda, leggins, Pantalón corto, Pantalón largo
-    CALZADO       - val tipo - botas, chanclas, sandalias, zapatos, zapatillas
-    ACCESORIOS    - val tipo - bufanda, bolso, cinturón, corbata, gorra, mochila, sombrero
-    INTERIOR      - val tipo - bragas, calzoncillos, sujetador
-    DE UNA PIEZA  - val tipo - mono, vestido
-
-ATRIBUTOS A AÑADIR:
-- colorSec: ninguno, (los colores del ppal)
- */
